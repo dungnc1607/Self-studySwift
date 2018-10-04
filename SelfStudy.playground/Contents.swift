9f5c110f5f5c9850b7d4d712e0e143e2357f9d20
Squall <dungnc1607@gmail.com>
2018-10-04 14:30:54 +0700

    Full

--- SelfStudy.playground/Contents.swift
+++ SelfStudy.playground/Contents.swift
@@ -56,4 +56,63 @@ fruits.isDisjoint(with: animals) //Khác hoàn toàn
 let myDict = ["Name":"Squall", "Age":"29", "Gender":"Male"]
 let allValues = [String](myDict.values)
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MARK: - For
+for i in stride(from: 1, to: 12, by: 3){
+    print("aloha ", i)
+}
+
+// - While
+var startValue = 0
+while startValue < 10{
+    startValue += 1
+    if startValue == 5 {
+        continue //ko check code trong mục này nữa
+    }
+    print(startValue)
+}
+
+var aString = "I am a man"
+print("String before: \(aString)")
+func inoutFunction(_ a: inout String){
+    a = "I am a hero"
+}
+inoutFunction(&aString)
+print("String after: \(aString)")
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MARK: - Closure
+var char = ["bcd", "ab", "xyz", "defg", "hi"]
+var sortCharacter = char.sorted { (s1: String, s2:String) -> Bool in
+    return s1 > s2 //sắp xếp theo điều kiện
+}
+
+let someNums = [1,2,3,4,5,6]
+var squaredNum = someNums.map { (someNum) -> Int in
+    return someNum * someNum
+}
+
+//Thay đổi giá trị ban đầu của struct, enum.. thêm mutating
+
+//Cách gọi closure trong switch case
+//vd:
+var things = [Any]()
+things.append({(name:String) -> String in "Hello, \(name)" })
+for thing in things{
+    switch thing {
+    case let stringInClosure as (String) -> String:
+        //do wtf you want
+        print("Squall no 1")
+    default:
+        print("smt else")
+    }
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//MARK: - Protocol - delegate
+//Delegate trao quyền cho 1 đối tượng nào đấy thực hiện 1 số chức năng nào đó, trong quá trình thực hiện thì phải gọi lại các phương thức trong protocol
+
+
+
 
